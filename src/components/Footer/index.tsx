0630b4f40028aff9389f9d092ed5598f97e990af
Iasmin <iasminraquelsou@gmail.com>
2024-10-02 23:12:48 -0300

    style: alterações no footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,25 +1,26 @@
 import style from './footer.module.scss'
 import Image from 'next/image'
+import logo_ufape from '../../../../public/logo_ufape.svg'
 
 function Footer() {
     return (
        <div className={style.footer}>
             <div className={style.footer__logo_sementes}>
-                <Image src="/assets/logoCoppabacsBranca.svg" alt="Logo Coppabacs" width={250} height={50}/>
+                <Image src="/logoSistema.svg" alt="Logo Coppabacs" width={250} height={50}/>
             </div>
             <div className={style.footer__logo_lmts}>
-                <Image src="/assets/logoUfape.svg" alt="Logo App" width={58} height={58}/>
-                <Image src="/assets/logoLmts.svg" alt="Logo App" width={99} height={42}/>  
+                <Image src="/logoUfape.svg"  alt="Logo ufape" width={58} height={58}/>
+                <Image src="/logoLMTS.svg" alt="Logo App" width={99} height={42}/>  
             </div>
             <div className={style.footer__social}>
                 <a href="#">
-                    <Image src="/assets/iconFacebook.svg" alt="Facebook Icon" width={24} height={24}/>
+                    <Image src="/facebookIcon.svg" alt="Facebook Icon" width={24} height={24}/>
                 </a>
                 <a href="#">
-                    <Image src="/assets/iconInstagram.svg" alt="Instagram Icon" width={24} height={24}/>
+                    <Image src="/instagramICon.svg" alt="Instagram Icon" width={27} height={26}/>
                 </a>
                 <a href="#">
-                    <Image src="/assets/iconEmail.svg" alt="Email Icon" width={27} height={26}/>
+                    <Image src="/gmailIcon.svg" alt="Email Icon" width={24} height={24}/>
                 </a>
             </div>
        </div>
