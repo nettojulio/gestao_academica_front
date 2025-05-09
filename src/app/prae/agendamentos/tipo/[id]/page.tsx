6c7be648d757f07a3146e426a49b0579c6b60c93
Douglas Morais <douglas.leitemorais@gmail.com>
2025-05-08 23:38:31 -0300

    fix: correções nas telas de tipos de atendimentos

--- src/app/prae/agendamentos/tipo/[id]/page.tsx
+++ src/app/prae/agendamentos/tipo/[id]/page.tsx
@@ -35,6 +35,7 @@ const cadastro = () => {
     return options;
   };
 
+
   const estrutura: any = {
     uri: "tipo-atendimento",
     cabecalho: {
@@ -56,10 +57,20 @@ const cadastro = () => {
           line: 1,
           colSpan: "md:col-span-1",
           nome: "Tipo Atendimento",
-          chave: "descricao",
+          chave: "nome",
           tipo: "text",
           mensagem: "Digite",
           obrigatorio: true,
+        },
+        {
+          line: 1,
+          colSpan: "md:col-span-1",
+          nome: "Tempo Atendimento",
+          chave: "tempoAtendimento",
+          tipo: "text",
+          mensagem: "Digite no padrão hh:mm",
+          obrigatorio: true,
+          
         },
         {
           line: 1,
@@ -118,6 +129,17 @@ const cadastro = () => {
    * fiquem agrupados em um objeto 'endereco'.
    */
   const salvarRegistro = async (item: any) => {
+    // Supondo que item.tempoAtendimento seja uma string no formato "HH:mm"
+    const [hora, minuto] = item.tempoAtendimento.split(':').map(Number);
+    const totalMinutos = hora * 60 + minuto;
+
+    if (totalMinutos > 60) {
+      toast.error("O tempo de atendimento não pode ser maior que 1 hora", {
+        position: "top-left"
+      });
+      return;
+    }
+    
     try {
       const body = {
         metodo: `${isEditMode ? "patch" : "post"}`,
@@ -145,6 +167,11 @@ const cadastro = () => {
         Swal.fire({
           title: "Tipo de atendimento registrado com sucesso!",
           icon: "success",
+          customClass: {
+            popup: "my-swal-popup",
+            title: "my-swal-title",
+            htmlContainer: "my-swal-html",
+          },
         }).then((result) => {
           if (result.isConfirmed) {
             chamarFuncao("voltar");
@@ -161,31 +188,43 @@ const cadastro = () => {
    * Localiza o registro para edição e preenche os dados
    */
   const editarRegistro = async (item: any) => {
-    try {
-      const body = {
-        metodo: "get",
-        uri: "/prae/" + estrutura.uri + "/" + item,
-        params: {},
-        data: item,
-      };
-      const response = await generica(body);
-      if (!response) throw new Error("Resposta inválida do servidor.");
-      if (response.data?.errors) {
-        Object.keys(response.data.errors).forEach((campoErro) => {
-          toast(`Erro em ${campoErro}: ${response.data.errors[campoErro]}`, {
-            position: "top-left",
-          });
+  try {
+    const body = {
+      metodo: "get",
+      uri: "/prae/" + estrutura.uri + "/" + item,
+      params: {},
+      data: item,
+    };
+    const response = await generica(body);
+    if (!response) throw new Error("Resposta inválida do servidor.");
+    if (response.data?.errors) {
+      Object.keys(response.data.errors).forEach((campoErro) => {
+        toast(`Erro em ${campoErro}: ${response.data.errors[campoErro]}`, {
+          position: "top-left",
         });
-      } else if (response.data?.error) {
-        toast.error(response.data.error.message, { position: "top-left" });
-      } else {
-        setDadosPreenchidos(response.data);
-      }
-    } catch (error) {
-      console.error("DEBUG: Erro ao localizar registro:", error);
-      toast.error("Erro ao localizar registro. Tente novamente!", { position: "top-left" });
+      });
+    } else if (response.data?.error) {
+      toast.error(response.data.error.message, { position: "top-left" });
+    } else {
+      const dados = response.data;
+
+      // Transformar 'horarios' para o formato "HH:mm"
+      dados.horarios = Array.isArray(dados.horarios)
+        ? dados.horarios.map((horario: string) => horario.slice(0, 5)) // Remove os segundos
+        : [];
+
+      // Transformar 'tempoAtendimento' para o formato "HH:mm"
+      dados.tempoAtendimento = dados.tempoAtendimento
+        ? dados.tempoAtendimento.slice(0, 5) // Remove os segundos
+        : "";
+
+      setDadosPreenchidos(dados);
     }
-  };
+  } catch (error) {
+    console.error("DEBUG: Erro ao localizar registro:", error);
+    toast.error("Erro ao localizar registro. Tente novamente!", { position: "top-left" });
+  }
+};
 
   // Efeito exclusivo para o modo de edição
   useEffect(() => {
